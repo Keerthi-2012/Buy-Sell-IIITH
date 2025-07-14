70d4e9537ac49c726997abca9f0b60b2a1a8dfa0
Keerthi Seela <seelakeerthi123@gmail.com>
2025-07-14 15:56:29 +0530

    Conclusion

--- BuySellIIITH/frontend/src/components/ItemUpdate.jsx
+++ BuySellIIITH/frontend/src/components/ItemUpdate.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Navbar from './shared/Navbar';
+import './ItemUpdate.css';
 
 const ItemUpdate = () => {
   const { id } = useParams();
